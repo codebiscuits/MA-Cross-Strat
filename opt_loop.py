0f37910e8b49398a92f4ec9791a82322972dd022
Ross Coates <ross_coates82@hotmail.com>
2020-03-05 10:11:15 +0000

    simplified opt_loop again. pretty sure now that the multiprocessing problems arise when there is anything else in the for loop with the optimisation. as long as opt_loop is the only item in the for loop, it seems to run ok. strategies.py now has three variations of MACross. The new one is MACrossRoot which has the ROC period set as the square root of whatever the ma period is, instead of the ROC period just being 1 in all cases. it occured to me that a fixed ROC period of 1 means that the stdev probably doesnt change much from shorter periods to very long periods, because the volatility between single 1m or 5m periods is probably always similar. so ROC period needs to lengthen as well as stdev period, just not as much, so that stdev is always calculating several ROC periods. so if root doesn't improve things, i could try fractions of stdev like 1/5, 1/10, 1/20 etc.

--- opt_loop.py
+++ opt_loop.py
@@ -2,13 +2,11 @@ import backtrader as bt
 import backtrader.feeds as btfeeds
 import datetime
 import time
-import os
 from sizers import PercentSizer
 import strategies
 import results_function_df as rf
 from pathlib import Path
 from binance.client import Client
-import pandas as pd
 import keys
 
 def get_pairs(quote):
@@ -20,39 +18,24 @@ def get_pairs(quote):
 
     for item in symbols:
         if item['symbol'][-length:] == quote:
-            if not (item['symbol'] in ['PAXUSDT', 'USDSBUSDT', 'BCHSVUSDT', 'BCHABCUSDT', 'VENUSDT', 'TUSDUSDT', 'USDCUSDT', 'USDSUSDT', 'BUSDUSDT', 'EURUSDT', 'BCCUSDT', 'IOTA']):
+            if not (item['symbol'] in ['PAXUSDT', 'USDSBUSDT', 'BCHSVUSDT', 'BCHABCUSDT', 'VENUSDT', 'TUSDUSDT', 'USDCUSDT', 'USDSUSDT', 'BUSDUSDT', 'EURUSDT', 'BCCUSDT', 'IOTAUSDT']):
                 pairs_list.append(item['symbol'])
 
     return pairs_list
 
-### optimisation params
-strat = strategies.MaCross
-s_n = strat.params.strat_name  # name of current strategy as a string for generating filenames etc
-timescale = '5m'
-start_date = datetime.datetime(2020, 1, 1)
-end_date = datetime.datetime(2020, 1, 31)
-ma = (2, 102)
-risk = (0, 80)
-step_size = 10
-pos_size = 25
+def opt_loop(pair):
 
-range_str = f'ma{ma[0]}-{ma[1]}_risk{risk[0]}-{risk[1]}_step{step_size}'
-date_range = f'{str(start_date)[:10]}_{str(end_date)[:10]}'
-
-t = 0
-
-def opt_loop(pair, strategy, timescale, start, end, ma, risk, step_size, pos_size):
-
+    ### optimisation params
     trading_pair = pair
-    strat = strategy
-    s_n = strat.params.strat_name
-    timescale = timescale
-    start_date = start
-    end_date = end
-    ma = ma
-    risk = risk
-    step_size = step_size
-    pos_size = pos_size
+    strat = strategies.MaCrossRoot
+    s_n = strat.params.strat_name  # name of current strategy as a string for generating filenames etc
+    timescale = '5m'
+    start_date = datetime.datetime(2020, 1, 1)
+    end_date = datetime.datetime(2020, 1, 31)
+    ma = (2, 1202)
+    risk = (200, 1800)
+    step_size = 20
+    pos_size = 25
     startcash = 1000
 
 
@@ -118,33 +101,7 @@ def opt_loop(pair, strategy, timescale, start, end, ma, risk, step_size, pos_siz
         else:
             print(f'Time elapsed: {int(t)}s')
 
-
 pairs = get_pairs('USDT')
 
-if not os.path.isdir(Path(f'Z:/results')):  # checks that the relevant folder exists
-    os.mkdir(Path(f'Z:/results'))  # creates the folder if it doesn't
-if not os.path.isdir(Path(f'Z:/results/{s_n}')):  # checks that the relevant folder exists
-    os.mkdir(Path(f'Z:/results/{s_n}'))  # creates the folder if it doesn't
-if not os.path.isdir(Path(f'Z:/results/{s_n}/{timescale}')):  # checks that the relevant folder exists
-    os.mkdir(Path(f'Z:/results/{s_n}/{timescale}'))  # creates the folder if it doesn't
-if not os.path.isdir(Path(f'Z:/results/{s_n}/{timescale}/{range_str}')):  # checks that the relevant folder exists
-    os.mkdir(Path(f'Z:/results/{s_n}/{timescale}/{range_str}'))  # creates the folder if it doesn't
-if not os.path.isdir(Path(f'Z:/results/{s_n}/{timescale}/{range_str}/size-{pos_size}')):  # checks that the relevant folder exists
-    os.mkdir(Path(f'Z:/results/{s_n}/{timescale}/{range_str}/size-{pos_size}'))  # creates the folder if it doesn't
-if not os.path.isdir(Path(f'Z:/results/{s_n}/{timescale}/{range_str}/size-{pos_size}/{date_range}')):  # checks that the relevant folder exists
-    os.mkdir(Path(f'Z:/results/{s_n}/{timescale}/{range_str}/size-{pos_size}/{date_range}'))  # creates the folder if it doesn't
-
 for pair in pairs:
-    if os.path.isfile(Path(f'Z:/results/{s_n}/{timescale}/{range_str}/size-{pos_size}/{date_range}/tests_started.csv')):
-        tests_started = pd.read_csv(f'Z:/results/{s_n}/{timescale}/{range_str}/size-{pos_size}/{date_range}/tests_started.csv')
-    else:
-        tests_started = pd.DataFrame({'pair': [], 'time': []})
-
-    started_list = list(tests_started['pair'])
-
-    if not pair in started_list:
-        tests_started = tests_started.append({'pair': pair, 'time': 0}, ignore_index=True)
-        tests_started.to_csv(Path(f'Z:/results/{s_n}/{timescale}/{range_str}/size-{pos_size}/{date_range}/tests_started.csv'))
-        opt_loop(pair, strat, timescale, start_date, end_date, ma, risk, step_size, pos_size)
-    tests_started.loc['pair' == pair, 'time'] = t
-    tests_started.to_csv(Path(f'Z:/results/{s_n}/{timescale}/{range_str}/size-{pos_size}/{date_range}/tests_started.csv'))
+    opt_loop(pair)
--- strategies.py
+++ strategies.py
@@ -1,4 +1,5 @@
 import backtrader as bt
+import math
 
 class MaCross(bt.Strategy):
 
@@ -25,6 +26,7 @@ class MaCross(bt.Strategy):
         self.ema = bt.ind.EMA(self.data.close, period=self.params.ma_periods)
         self.sma = bt.ind.SMA(self.data.close, period=self.params.ma_periods)
         self.roc = bt.ind.ROC(self.data.close, period=1)
+        # TODO possible improvement might be to adjust roc periods as well as stdev periods to give more true measure of recent volatility. maybe set roc periods as sqrt of ma_periods
         self.volatil = bt.ind.StdDev(self.roc, period=self.params.ma_periods)
         self.buysig = bt.ind.CrossOver(self.ema, self.sma)
         self.sellsig = bt.ind.CrossOver(self.sma, self.ema)
@@ -100,6 +102,106 @@ class MaCross(bt.Strategy):
                 self.order = self.sell()
                 self.order = self.buy(exectype=bt.Order.StopTrail, trailpercent=self.stop_band)
 
+class MaCrossRoot(bt.Strategy):
+    params = (
+        ('strat_name', 'macrossroot'),
+        ('ma_periods', 60),
+        ('vol_mult', 1),
+        ('pos_size', 99),
+        ('start', 0),
+        ('debug', False),
+    )
+
+    def log(self, txt, dt=None):
+        dt = dt or self.datas[0].datetime.date(0)
+        # print('%s, %s' % (dt.isoformat(), txt))
+
+    def __init__(self):
+        self.startcash = self.broker.getvalue()
+        self.dataclose = self.datas[0].close
+        self.order = None
+        self.buyprice = None
+        self.buycomm = None
+
+        self.root_ma = round(math.sqrt(self.ma_periods))
+
+        self.ema = bt.ind.EMA(self.data.close, period=self.params.ma_periods)
+        self.sma = bt.ind.SMA(self.data.close, period=self.params.ma_periods)
+        self.roc = bt.ind.ROC(self.data.close, period=self.root_ma)
+        # TODO if root_ma doesnt improve stuff, maybe some other setting will
+        self.volatil = bt.ind.StdDev(self.roc, period=self.params.ma_periods)
+        self.buysig = bt.ind.CrossOver(self.ema, self.sma)
+        self.sellsig = bt.ind.CrossOver(self.sma, self.ema)
+        self.stop_band = self.params.vol_mult * self.volatil * 0.01
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            # Nothing to do since order was submitted/accepted to/by broker
+            return
+
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log(
+                    'BUY EXECUTED, Price: %.2f, Cost: %.2f, Comm: %.2f' %
+                    (order.executed.price,
+                     order.executed.value,
+                     order.executed.comm))
+
+                self.buyprice = order.executed.price
+                self.buycomm = order.executed.comm
+            else:
+                self.log('SELL EXECUTED, Price: %.2f, Cost: %.2f, Comm: %.2f' %
+                         (order.executed.price,
+                          order.executed.value,
+                          order.executed.comm))
+
+            self.bar_executed = len(self)
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]:
+            self.log('Order Canceled/Margin/Rejected')
+
+        self.order = None
+
+    def notify_trade(self, trade):
+        if not trade.isclosed:
+            return
+
+        self.log('OPERATION PROFIT, GROSS %.2f, NET %.2f' %
+                 (trade.pnl, trade.pnlcomm))
+
+    def next(self):
+        self.log('Close, %.2f' % self.dataclose[0])
+
+        if self.order:
+            return
+
+        if self.buysig and not self.position:
+
+            self.log('BUY CREATE, %.2f' % self.dataclose[0])
+            self.order = self.buy()
+            self.order = self.sell(exectype=bt.Order.StopTrail, trailpercent=self.stop_band)
+
+        elif self.sellsig and not self.position:
+
+            self.log('SELL CREATE, %.2f' % self.dataclose[0])
+            self.order = self.sell()
+            self.order = self.buy(exectype=bt.Order.StopTrail, trailpercent=self.stop_band)
+
+        elif self.buysig and self.position:
+
+            if self.position.size < 0:
+                self.log('BUY CREATE, %.2f' % self.dataclose[0])
+                self.order = self.close()
+                self.order = self.buy()
+                self.order = self.sell(exectype=bt.Order.StopTrail, trailpercent=self.stop_band)
+
+        elif self.sellsig and self.position:
+
+            if self.position.size > 0:
+                self.log('SELL CREATE, %.2f' % self.dataclose[0])
+                self.order = self.close()
+                self.order = self.sell()
+                self.order = self.buy(exectype=bt.Order.StopTrail, trailpercent=self.stop_band)
 
 class MaCrossRSI(bt.Strategy):
     params = (
